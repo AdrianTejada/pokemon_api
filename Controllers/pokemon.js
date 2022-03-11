9a61e78ffb37f33f47c626ed9b4c0dbdc26a21ce
AdrianTejada <atejada@my.bcit.ca>
2022-03-11 14:00:08 -0800

    request.body is working

--- Controllers/pokemon.js
+++ Controllers/pokemon.js
@@ -1,12 +1,11 @@
 const Pokemon = require('../Models/pokemon.js')
 
 const GetPokemon = (req, res) => {
+  const {txt, gen1, gen2, gen3, gen4, gen5, gen6, show_legendaries } = req.body;
+
   Pokemon.find({},(err,data)=>{
     res.json(data)
-    
   })
 }
 
-module.exports = {
-  GetPokemon
-}
+module.exports = {GetPokemon}
--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./config')
-
 const router = require('./Routes/pokemon.js')
 
 mongoose.connect(config.MONGODB_URL, (e)=>{
-  console.log(e)
-})
-
-app.get('/', (req,res)=>{
-  res.json({'hello':'hello'})
+  if (e){
+    console.log(e)
+  }
 })
 
 app.use(express.json())
-
 app.use(router)
-
-app.listen(3000, console.log('app running'))
+app.listen(8888, console.log('app running'))
