60ddbf93a6b2568207e11e5539aeb6c416362616
AdrianTejada <atejada@my.bcit.ca>
2022-03-14 15:39:09 -0700

    getting pokemon works

--- Controllers/pokemon.js
+++ Controllers/pokemon.js
@@ -1,11 +1,53 @@
 const Pokemon = require('../Models/pokemon.js')
 
 const GetPokemon = (req, res) => {
-  const {txt, gen1, gen2, gen3, gen4, gen5, gen6, show_legendaries } = req.body;
+  var {txt, gen1, gen2, gen3, gen4, gen5, gen6, show_legendaries } = req.body;
 
-  Pokemon.find({},(err,data)=>{
-    res.json(data)
-  })
+  txt = txt.charAt(0).toUpperCase() + txt.slice(1)
+
+  if (!show_legendaries) {
+    Pokemon.find({ $and: [
+      {$or: [
+        {Name: {$regex: txt}},
+        {Type1: {$regex: txt}},
+        {Type2: {$regex: txt}}
+      ]},
+      {$or:[
+        {Generation: gen1},
+        {Generation: gen2},
+        {Generation: gen3},
+        {Generation: gen4},
+        {Generation: gen5},
+        {Generation: gen6}
+      ]}
+    ]},(err,data)=>{
+      if (err) {
+        console.log
+      } 
+      res.json(data)
+    })
+  } else {
+    Pokemon.find({ $and: [
+      {$or: [
+        {Name: {$regex: txt}},
+        {Type1: {$regex: txt}},
+        {Type2: {$regex: txt}}]},
+      {$or:[
+        {Generation: gen1},
+        {Generation: gen2},
+        {Generation: gen3},
+        {Generation: gen4},
+        {Generation: gen5},
+        {Generation: gen6}
+      ]},
+      {Legendary: show_legendaries}
+  ]},(err,data)=>{
+      if (err) {
+        console.log
+      } 
+      res.json(data)
+    })
+  }
 }
 
 module.exports = {GetPokemon}
--- Routes/pokemon.js
+++ Routes/pokemon.js
@@ -2,6 +2,6 @@ const {GetPokemon} = require('../Controllers/pokemon.js')
 const express = require('express')
 const router = express.Router()
 
-router.get('/pokemon', GetPokemon)
+router.get('/getpokemon', GetPokemon)
 
 module.exports = router;
