ff0787d367294f2273dd85010b8fbe618dde5b3d
AdrianTejada <atejada@my.bcit.ca>
2022-03-17 19:39:42 -0700

    drag and drop works with sockets

--- Routes/pokemon.js
+++ Routes/pokemon.js
@@ -3,7 +3,6 @@ const authoriseUser = require('../Auth/auth')
 const express = require('express')
 const router = express.Router()
 
-// router.get('/getpokemon', authoriseUser, GetPokemon)
-router.get('/getpokemon', GetPokemon)
+router.get('/getpokemon', authoriseUser, GetPokemon)
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,17 +2,65 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const http = require('http')
+const server = http.createServer(app)
+const {Server} = require('socket.io')
+
 const config = require('./config')
 const pokemonRouter = require('./Routes/pokemon.js')
 const userRouter = require('./Routes/user.js')
+
+const io = new Server(server, {
+  cors:{origin:['http://localhost:3000'],
+  allowedHeaders:["Access-Control-Allow-Origin"],
+  credentials: true
+  }
+})
+
+
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(config.MONGODB_URL, (e)=>{
   if (e){
     console.log(e)
   }
 })
+
 app.use(cors())
 app.use(express.json())
 app.use(pokemonRouter)
 app.use(userRouter)
-app.listen(process.env.PORT || 5000, console.log('running'))
+
+const users = {};
+
+const Pokemon1 = null;
+
+const Pokemon2 = null;
+
+io.on('connection', (socket)=>{
+  console.log('a user connected')
+
+  socket.on('user_joined', (username)=>{
+    users[socket.id] = {username}
+    console.log(users)
+  })
+
+  socket.on('disconnect', ()=>{
+    delete users[socket.id]
+  })
+
+  socket.on('pokemon_1', (pokemon1)=> {
+    io.emit('setpokemon_1', pokemon1);
+  })
+
+  socket.on('pokemon_2', (pokemon2)=> {
+    io.emit('setpokemon_2', pokemon2);
+  })
+
+  socket.on('reset', ()=>{
+    io.emit('setReset', null)
+  })
+})
+
+
+server.listen(PORT, console.log(`running on *:${PORT}`))
